897bdf2b838ea2bbb0e0a3230239a4c69fcde232
muztechiee <muztechiee@gmail.com>
2025-11-01 22:32:32 +0500

    Task6

---
+++ Task6.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> fruits = ["Apple", "Mango", "Banana"];
+
+  List<String> reversed = fruits.reversed.toList();
+
+  print(reversed);
+  print(fruits); // original same
+}
